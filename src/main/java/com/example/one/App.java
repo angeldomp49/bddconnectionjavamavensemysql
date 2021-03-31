46272df2f8fc28315b6a040795b73f46c8af0205
Angel Domínguez Plata <angeldomp49@gmail.com>
2021-03-31 09:41:30 -0600

    testing bdd connection

--- src/main/java/com/example/one/App.java
+++ src/main/java/com/example/one/App.java
@@ -3,7 +3,10 @@ package com.example.one;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 /**
  * Hello world!
  *
@@ -16,15 +19,37 @@ public class App
         String server_url = "//localhost:3306";
         String bd_name = "/for_dates";
 
+        Connection DB = null;
+
         String complete_url = driver_url + server_url + bd_name;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println( "Driver OK!" );
-            DriverManager.getConnection(complete_url, "root", "");
-            System.out.println("Connection OK!");
+            DB = DriverManager.getConnection(complete_url, "root", "");
+            System.out.print("connection OK!");
+            Statement bddCommand = DB.createStatement();
+            ResultSet bddResponse = bddCommand.executeQuery("SELECT * FROM comments");
+            ResultSetMetaData commentsMetaData = bddResponse.getMetaData();
+            System.out.println("\n**********************************");
+
+            for( int i = 1; i <= commentsMetaData.getColumnCount(); i++ ){
+                System.out.print("\t" + commentsMetaData.getColumnName(i).toString() + "\t |");
+            }
+
+            System.out.println("\t****************************************");
+
+            while( bddResponse.next() ){
+                for( int i = 1; i <= commentsMetaData.getColumnCount(); i++ ){
+                    System.out.print("\t" + bddResponse.getObject(i).toString() + "\t |");
+                }
+                System.out.println("\n---------------------------------");
+            }
+
+            bddResponse.close();
+            bddCommand.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         System.out.println("program finished!");
         
     }
